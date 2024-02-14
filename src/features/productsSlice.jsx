3642c8e0e97d141c50ddaccef7f6101766f525ac
argun8394 <argundoganali@gmail.com>
2024-02-14 12:04:24 +0300

    up

--- src/features/productsSlice.jsx
+++ src/features/productsSlice.jsx
@@ -27,9 +27,6 @@ const productsSlice = createSlice({
       state.products = [];
     },
   },
-  //? createAyncThunk metedo bir middleware olarak API gibi dis kaynakli isteklerin redux ortaminda olsuturulmasini saglar. Ancak API^deki durumlara gore state'lerin guncellenmesini saglamaz. Bunun icin slice icersiindeki extraReducer kismi kullanilir.
-
-  //? API isteklerinde 3 farkli alt durum meydana gelir. Bunlar baslama (pending), basarili bitme (fullfilled) ve basariz bitme (rejected) dir.
 
   extraReducers: (builder) => {
     builder
